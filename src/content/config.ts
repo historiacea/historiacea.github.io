0f247b25c66dd078f3869335cc32826856283684
Historia Cea <daevacp@gmail.com>
2025-08-19 22:55:29 +0200

    chore: enable GitHub Pages deployment

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,8 @@
 import { defineCollection } from 'astro:content';
-import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { docsSchema } from '@astrojs/starlight/schema';
 
 const docs = defineCollection({
   schema: docsSchema(),
 });
 
-const i18n = defineCollection({
-  type: 'data',
-  schema: i18nSchema(),
-});
-
-export const collections = { docs, i18n };
+export const collections = { docs };
